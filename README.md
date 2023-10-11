99289eb4b438cad2b50a2ef4318e586f6c4be3c5
Arpan-mukherje <mukherjee.arpan@hotmail.com>
2023-10-11 21:21:14 +0530

    Updated

---
+++ README.md
@@ -0,0 +1,1 @@
+![Collage Maker-16-Apr-2023-10-44-AM-4408](https://user-images.githubusercontent.com/99496645/232274474-d7a46c59-5e66-45a8-993c-01a1276e559c.jpg)
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_using_news_api/ui/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+          appBarTheme:
+              const AppBarTheme(backgroundColor: Color.fromARGB(255, 169, 97, 4))),
+      home: const Home(),
+    );
+  }
+}
